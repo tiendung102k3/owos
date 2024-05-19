48e742aafd4f8097da0d8a909914810c7139c768
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2024-05-19 09:16:25 +0700

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,59 +1,61 @@
-import logging
+from aiohttp import ClientSession, CookieJar
 
-from nicegui import ui
-
-
-class Weblogger(logging.Handler):
-    """A logging handler that emits messages to a log element."""
-
-    def __init__(self, element: ui.log, level: int = logging.NOTSET) -> None:
-        self.element = element
-        super().__init__(level)
-
-    def emit(self, record: logging.LogRecord) -> None:
-        try:
-            msg = self.format(record)
-            self.element.push(msg)
-        except Exception:
-            self.handleError(record)
+from src.logger import logger
 
 
-class Formatter(logging.Formatter):
-    """A Custom Formatter Class For Logging"""
-
-    # Escape Color Character
-    GREY = "\x1b[38;5;240m"
-    YELLOW = "\x1b[0;33m"
-    CYAN = "\x1b[1;94m"
-    RED = "\x1b[1;31m"
-    BRIGHT_RED = "\x1b[1;41m"
-    RESET = "\x1b[0m"
-    FORMAT = "\x1b[0;43m%(asctime)s\x1b[0m - {}%(levelname)s{} - %(message)s"
-
-    # Format Color For Each Level
-    FORMATS = {
-        logging.DEBUG: FORMAT.format(GREY, RESET) + "(%(filename)s:%(lineno)d)",
-        logging.INFO: FORMAT.format(CYAN, RESET),
-        logging.WARNING: FORMAT.format(YELLOW, RESET),
-        logging.ERROR: FORMAT.format(RED, RESET) + "(%(filename)s:%(lineno)d)",
-        logging.CRITICAL: FORMAT.format(BRIGHT_RED, RESET)
-        + "(%(filename)s:%(lineno)d)",
+async def submit_oauth(res):
+    response = await res.json()
+    locauri = response.get("location")
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", "accept-encoding": "gzip, deflate, br", "accept-language": "en-US,en;q=0.5", "connection": "keep-alive",
+        "host": "owobot.com",
+        "referer": "https://discord.com/", "sec-fetch-dest": "document", "sec-fetch-mode": "navigate", "sec-fetch-site": "cross-site", "sec-fetch-user": "?1", "upgrade-insecure-requests": "1", "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0"
     }
+    logger.warning(locauri)
 
-    def format(self, record):
-        """
-        Format Function (Method)
-        """
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt, "%d %b %Y %H:%M:%S")
-        return formatter.format(record)
+    session = ClientSession(cookie_jar=CookieJar())
 
-# SETTING UP LOGGER
+    async with session.get(locauri, headers=headers, allow_redirects=False) as res2:
+        if res2.status in (302, 307):
+            logger.warning("(+) Token Added To OAuth")
+            return session
+        else:
+            return print(f"(-) Failed to add token to oauth | {res2.status}")
 
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-ch.setFormatter(Formatter())
-logger.addHandler(ch)
+async def get_oauth(token):
+    async with ClientSession() as session:
+        oauth = "https://discord.com/api/v9/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fowobot.com%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=identify%20guilds%20email%20guilds.members.read&client_id=408785106942164992"
+
+        payload = {
+            "permissions": "0",
+            "authorize": True
+        }
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0',
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'application/json',
+            'Authorization': token,
+            'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjEwOS4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzExMS4wIiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTExLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTg3NTk5LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==',
+            'X-Debug-Options': 'bugReporterEnabled',
+            'Origin': 'https://discord.com',
+            'Connection': 'keep-alive',
+            'Referer': "https://discord.com//oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fowobot.com%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=identify%20guilds%20email%20guilds.members.read&client_id=408785106942164992",
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'TE': 'trailers',
+        }
+
+        async with session.post(oauth, headers=headers, json=payload) as res:
+            if res.status == 401:
+                print(f"(!) Invalid Token [{token[:25]}...]")
+            elif res.status == 200:
+                result_session = await submit_oauth(res)
+                logger.warning(result_session)
+                return result_session
+            else:
+                print(f"(!) Error | {await res.text()}")
